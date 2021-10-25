2d133e1d45d364548fb7e04bf0144afb50c6be93
alheym <alheym.mw@gmail.com>
2021-10-25 16:10:45 +0300

    changed icons in the contact block

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,5 +10,27 @@ const hamburger = document.querySelector('.hamburger'),
           menu.classList.remove('active');
       });
 
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        // $('#consultation, #order').fadeOut();
+        // $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
       
 
